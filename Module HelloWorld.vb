41d0efc4f21d5e15f0f9ebd80cd87ce4f7b7b0eb
shrikt <103482252+shrikt@users.noreply.github.com>
2022-05-03 11:00:09 +0530

    Add files via upload

---
+++ Module HelloWorld.vb
@@ -0,0 +1,13 @@
+ Module HelloWorld
+	 Sub Main()
+	Dim firstNum,seconNum,sum As Integer
+Console.WriteLine("enter first number:")
+firstNum=Console.ReadLine()
+Console.WriteLine(" enter second number:")
+seconNum =Console.ReadLine()
+sum = firstNum + seconNum
+Console.WriteLine("the sum is:" & sum)
+Console.ReadLine()
+	
+	End Sub
+End Module
